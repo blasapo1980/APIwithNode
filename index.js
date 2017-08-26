6fc612c966779f02760ff402da77a42416ddebb6
Blasapo <blasapo@gmail.com>
2017-08-26 11:56:44 +0200

    basic server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict'
 
-var express = require("express");
-var bodyParser  = require("body-parser");
-var mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+  console.log('servidor escuchando en puerto 3000');
+});
