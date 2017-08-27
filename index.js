49fcc29caaff6e1229a89ba13f2fe32805d3f9f6
Blasapo <blasapo@gmail.com>
2017-08-27 18:08:45 +0200

    add true to fav

--- index.js
+++ index.js
@@ -9,40 +9,48 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 let notes_collection = {};
-//GET
+
+//GET: Para que devuelva todas las frases
 app.get('/api/notes', (req, res) => {
   let all_notes = notes_collection;
   res.status(200).send({notes: all_notes});
 });
 
-  //para una frase
+//para que devuelva una frase (según el ID)
 app.get('/api/notes/:noteId', (req, res) => {
   let noteId = req.params.noteId;
   res.status(200).send(notes_collection[noteId]);
-
 });
 
-//POST: crear notas. Crear favorita
+//POST: crear notas, darle un ID y almacenarlas.
 app.post('/api/notes', (req, res) => {
   console.log(req.body);
   let note = req.body.note;
   let last_id = Object.keys(notes_collection).length;
   let id = last_id + 1;
-  notes_collection[id] = { note: note };
+  let fav = req.body.fav||false;
+  notes_collection[id] = {note: note, fav};
   res.status(200).send({message:`La frase ${note} ha sido almacenada`});
 });
 
-app.post('/api/favorites', (req,res) => {
-
+//PUT. modificar una nota: marcar una nota como favorita
+app.put('/api/notes/fav/:noteId', (req,res) => {
+  let noteId = req.params.noteId;
+  notes_collection[noteId].fav === true;
+  res.status(200).send({message: `La frase ${notes_collection[noteId].note} ha sido guardada como favorita`});
 });
 
 //GET: consultar todas las favoritas
-app.get('/api/favorites', (req,res) => {
-
+app.get('/api/notes/fav', (req,res) => {
+  let notes_favorites = [];
+  for (var i = 0; i<notes_collection.length; i++) {
+    if(notes_collection[i].fav === true){
+      notes_favorites.push(notes_collection[i]);
+    }
+  }
+  res.status(200).send(notes_favorites);
 });
-//PUT
-
 
 app.listen(3000, () => {
-  console.log('servidor escuchando en puerto 3000');
+  console.log('Servidor escuchando en puerto 3000');
 });
